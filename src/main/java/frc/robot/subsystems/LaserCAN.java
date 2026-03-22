ca4079d2577461de988386a9c37ff5ac0e56af8f
FuyuYuki125 <leafytree125@gmail.com>
2026-03-22 16:53:35 -0400

    LaserCAN: Not finished

--- src/main/java/frc/robot/subsystems/LaserCAN.java
+++ src/main/java/frc/robot/subsystems/LaserCAN.java
@@ -1,4 +1,4 @@
-package frc.robot.Subsystems;
+package frc.robot.subsystems;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +14,11 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
-import frc.robot.Subsystems.Drivetrain.DriveConstants;
+import frc.robot.Robot;
+import frc.robot.subsystems.drive.DriveConstants;
 
 public class LaserCAN extends SubsystemBase {
-    RobotContainer robot;
+    Robot robot;
     List<LaserCan> LCs = new ArrayList<>();
 
     private final ProfiledPIDController controller = new ProfiledPIDController(
@@ -27,7 +27,7 @@ public class LaserCAN extends SubsystemBase {
                         0.0, 
         new TrapezoidProfile.Constraints(1.0, 0.5)); // 1 m/s, 0.5 m/s²
 
-    public LaserCAN(RobotContainer robot) {
+    public LaserCAN(Robot robot) {
         this.robot = robot;
 
         LCs.add(new LaserCan(0));
@@ -74,7 +74,7 @@ public class LaserCAN extends SubsystemBase {
                                         DriveConstants.kMaxAngularSpeed,
                                         DriveConstants.kMaxAngularAcceleration),
                         0.02);
-        Pose2d currentPose = robotState.getLatestFieldToRobot().getValue();
+        //Pose2d currentPose = robotState.getLatestFieldToRobot().getValue();
         double tolerance = 40;
         double targetDistance = 10;
 
@@ -86,36 +86,37 @@ public class LaserCAN extends SubsystemBase {
         SmartDashboard.putNumber("LaserCAN/0CAN", leftDist);
         SmartDashboard.putNumber("LaserCAN/1CAN", rightDist);
 
-        if (leftDist != -1 || rightDist != -1) {
-            if (leftDist > targetDistance || rightDist > targetDistance) {
-                double targetPosition = Math.max(leftDist, rightDist) - targetDistance; // meters
-                robot.drive.setRobotSpeeds(new ChassisSpeeds(forwardSpeed, 0, 0));
-                SmartDashboard.putString("LaserCAN/Command", "Drive Forward");
-                climbAlignController.reset(
-                                currentPose.getRotation().getRadians(),
-                                robotState.getLatestRobotRelativeChassisSpeed().omegaRadiansPerSecond);
+        //Fix later
+        // if (leftDist != -1 || rightDist != -1) {
+        //     if (leftDist > targetDistance || rightDist > targetDistance) {
+        //         double targetPosition = Math.max(leftDist, rightDist) - targetDistance; // meters
+        //         //robot.drive.setRobotSpeeds(new ChassisSpeeds(forwardSpeed, 0, 0));
+        //         SmartDashboard.putString("LaserCAN/Command", "Drive Forward");
+        //         climbAlignController.reset(
+        //                         currentPose.getRotation().getRadians(),
+        //                         robotState.getLatestRobotRelativeChassisSpeed().omegaRadiansPerSecond);
                 
-            } else{
-                if (leftDist == -1 && rightDist != -1) {
-                    robot.drive.setRobotSpeeds(new ChassisSpeeds(0, strafeSpeed, 0));
-                    SmartDashboard.putString("LaserCAN/Command", "Drive Left");
-                } else if (rightDist == -1 && leftDist != -1) {
-                    robot.drive.setRobotSpeeds(new ChassisSpeeds(0, -strafeSpeed, 0));
-                    SmartDashboard.putString("LaserCAN/Command", "Drive Right");
-                } else if (rightDist != -1 && leftDist != -1) {
-                    if (leftDist + tolerance < rightDist) {
-                        robot.drive.setRobotSpeeds(new ChassisSpeeds(0, 0, rotationSpeed));
-                        SmartDashboard.putString("LaserCAN/Command", "Turn Left");
-                    } else if (rightDist + tolerance < leftDist) {
-                        robot.drive.setRobotSpeeds(new ChassisSpeeds(0, 0, -rotationSpeed));
-                        SmartDashboard.putString("LaserCAN/Command", "Turn Right");
-                    } 
-                } else {
-                    robot.drive.setRobotSpeeds(new ChassisSpeeds(0, 0, 0));
-                    SmartDashboard.putString("LaserCAN/Command", "Align complete");
-                }
-            }
-        }
+        //     } else{
+        //         if (leftDist == -1 && rightDist != -1) {
+        //             robot.drive.setRobotSpeeds(new ChassisSpeeds(0, strafeSpeed, 0));
+        //             SmartDashboard.putString("LaserCAN/Command", "Drive Left");
+        //         } else if (rightDist == -1 && leftDist != -1) {
+        //             robot.drive.setRobotSpeeds(new ChassisSpeeds(0, -strafeSpeed, 0));
+        //             SmartDashboard.putString("LaserCAN/Command", "Drive Right");
+        //         } else if (rightDist != -1 && leftDist != -1) {
+        //             if (leftDist + tolerance < rightDist) {
+        //                 robot.drive.setRobotSpeeds(new ChassisSpeeds(0, 0, rotationSpeed));
+        //                 SmartDashboard.putString("LaserCAN/Command", "Turn Left");
+        //             } else if (rightDist + tolerance < leftDist) {
+        //                 robot.drive.setRobotSpeeds(new ChassisSpeeds(0, 0, -rotationSpeed));
+        //                 SmartDashboard.putString("LaserCAN/Command", "Turn Right");
+        //             } 
+        //         } else {
+        //             robot.drive.setRobotSpeeds(new ChassisSpeeds(0, 0, 0));
+        //             SmartDashboard.putString("LaserCAN/Command", "Align complete");
+        //         }
+        //     }
+        // }
 
         // if (leftDist == -1 && rightDist != -1) {
         //     robot.drive.setRobotSpeeds(new ChassisSpeeds(strafeSpeed, 0, 0));
