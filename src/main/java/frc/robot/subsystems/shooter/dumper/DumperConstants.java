b0ebdecaee84397179eed577d4c12546659368e2
varsha <varshasukumar25@gmail.com>
2026-08-27 17:47:07 -0400

    savew my dumper thingy?

--- src/main/java/frc/robot/subsystems/shooter/dumper/DumperConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/dumper/DumperConstants.java
@@ -3,25 +3,32 @@ package frc.robot.subsystems.shooter.dumper;
 public class DumperConstants {
     public static final int kDumperCanId = 15;
 
+    //PID is for the tuning the motor part i thinkk
     public static final double kDumperP = 0.5;
     public static final double kDumperI = 0;
     public static final double kDumperD = 0;
-    public static final double kDumperS = 0;
-    public static final double kDumperV = 0;
-    public static final double kDumperA = 0;
-    public static final double kDumperG = 0;
+
+    //SVAG, is for static friction, velocity, acceleration and gravity, something like that
+    public static final double kDumperS = 0, kDumperV = 0, kDumperA = 0, kDumperG = 0;
     public static final double kDumperMaxAccel = 800.0;
     public static final double kDumperCruiseVel = 600.0;
     public static final double kDumperDeviationErr = 1;
 
+    public static final double kDumperSimP = 0.1;
+    public static final double kDumperSimD = 0;
+
+    // radians per motor rotation, (turret value honestly)
     public static final double kDumperPositionConversionFactor = 2.0 * Math.PI / 4.0 / (200.0 / 20.0);
+
+    //radians per second
     public static final double kDumperVelocityConversionFactor = kDumperPositionConversionFactor / 60;
 
     public static final boolean kDumperInverted = false;
     public static final int kDumperCurrentLimit = 40;
-
     public static final double latencyCompensationMS = 20.0;
 
-    public static final double kForwardSoftLimit = Math.PI / 2;
-    public static final double kBackwardSoftLimit = 0.0;
+    public static final double kForwardSoftLimit = Math.PI/2;
+    public static final double kBackwardSoftLimit = 0.0;  
+
+
 }
--- src/main/java/frc/robot/subsystems/shooter/dumper/DumperIO.java
+++ src/main/java/frc/robot/subsystems/shooter/dumper/DumperIO.java
@@ -2,28 +2,46 @@ package frc.robot.subsystems.shooter.dumper;
 
 import org.littletonrobotics.junction.AutoLog;
 
-public interface DumperIO {
-  @AutoLog
-  public static class DumperIOInputs {
-    public double dumperPos = 0.0;
-    public double dumperVelRadPerSec = 0.0;
-    public double appliedVolts = 0.0;
-    public double currentAmps = 0.0;
-    public double desiredPos = 0.0;
-    public boolean isDumping = false;
-  }
+public class DumperIO {
 
-  default void updateInputs(DumperIOInputs inputs) {}
+    @AutoLog
+    public static class DumperIOInputs{
+        public double dumperPos = 0.0;
+        public double dumperVelRadPerSec = 0.0;
 
-  public default void setDumperVoltage(double volts) {}
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
 
-  public default void setDumperPosition(double position, double ff) {}
+        public double desiredPos = 0.0;
 
-  public default void setDumperPosition(double position) {}
+        public boolean isDumping = false;
+    }
+    
 
-  public default double getDumperPosition() { return 0.0; }
+    public default void updateInputs(DumperIOInputs inputs) {}
 
-  public default double getDumperVelocity() { return 0.0; }
+    public default void setDumperVoltage(double volts){
 
-  public default void stopDumper() {}
+    }
+    public default void setDumperPosition(double position, double ff) {
+
+    }
+    public default void setDumperPosition(double position) {
+
+    }
+    public default double getDumperPosition(){ 
+        return 0.0;
+    }
+    public default double getDumperVelocity() {
+        return 0.0;
+    }
+    public default void stopDumper(){
+        
+    }
 }
+
+
+
+
+
+
